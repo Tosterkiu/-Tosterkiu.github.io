bbf5dd1ee80bc2bdf80f4c3953635a2a22e18b3d
Tosterkiu <83880082+Tosterkiu@users.noreply.github.com>
2023-04-15 19:35:11 +0300

    Update upload.php

--- upload.php
+++ upload.php
@@ -1,35 +1,50 @@
+
 <?php
+$target_dir = "uploads/";
+$target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
+$uploadOk = 1;
+$imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
 
-// Get reference to uploaded image
-$image_file = $_FILES["image"];
-
-// Exit if no file uploaded
-if (!isset($image_file)) {
-    die('No file uploaded.');
+// Check if image file is a actual image or fake image
+if(isset($_POST["submit"])) {
+  $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
+  if($check !== false) {
+    echo "File is an image - " . $check["mime"] . ".";
+    $uploadOk = 1;
+  } else {
+    echo "File is not an image.";
+    $uploadOk = 0;
+  }
 }
 
-// Exit if image file is zero bytes
-if (filesize($image_file["tmp_name"]) <= 0) {
-    die('Uploaded file has no contents.');
+// Check if file already exists
+if (file_exists($target_file)) {
+  echo "Sorry, file already exists.";
+  $uploadOk = 0;
 }
 
-// Exit if is not a valid image file
-$image_type = exif_imagetype($image_file["tmp_name"]);
-if (!$image_type) {
-    die('Uploaded file is not an image.');
+// Check file size
+if ($_FILES["fileToUpload"]["size"] > 500000) {
+  echo "Sorry, your file is too large.";
+  $uploadOk = 0;
 }
 
-// Get file extension based on file type, to prepend a dot we pass true as the second parameter
-$image_extension = image_type_to_extension($image_type, true);
+// Allow certain file formats
+if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
+&& $imageFileType != "gif" ) {
+  echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
+  $uploadOk = 0;
+}
 
-// Create a unique image name
-$image_name = bin2hex(random_bytes(16)) . $image_extension;
-
-// Move the temp image file to the images directory
-move_uploaded_file(
-    // Temp image location
-    $image_file["tmp_name"],
-
-    // New image location
-    __DIR__ . "/images/" . $image_name
-);
+// Check if $uploadOk is set to 0 by an error
+if ($uploadOk == 0) {
+  echo "Sorry, your file was not uploaded.";
+// if everything is ok, try to upload file
+} else {
+  if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
+    echo "The file ". htmlspecialchars( basename( $_FILES["fileToUpload"]["name"])). " has been uploaded.";
+  } else {
+    echo "Sorry, there was an error uploading your file.";
+  }
+}
+?>
